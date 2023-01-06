e27d7e56b03dbb2b580f51a46495c11686445b55
Alex Bodrov <alexvbodrov@mail.ru>
2023-01-07 00:00:32 +0300

    new file

---
+++ stepik/st_example_21.py
@@ -0,0 +1,9 @@
+letters = [c for c in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789']
+morse = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..', '-----', '.----', '..---', '...--', '....-', '.....', '-....', '--...', '---..', '----.']
+mydict = dict(zip(letters, morse))
+# print(mydict)
+word = 'Interstellar'.upper() # input().upper()
+
+for c in word:
+    if c in mydict:
+        print(mydict[c], end=' ')
